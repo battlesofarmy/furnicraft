aa8afe29f55de8eb6b0f40245fe6f4f86d6e9cb5
battlesofarmy <battlesofarmy@gmail.com>
2025-03-15 19:58:45 +0600

    skeleton added

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -4,6 +4,8 @@ import { useQuery, gql } from "@apollo/client";
 import Image from "next/image";
 import Link from "next/link";
 import CartWishlistIcon from '@/components/CartWishlistIcon';
+import { Skeleton } from "@/components/ui/skeleton";
+
 
 const ALL_SUBCATEGORY = gql`
  query($subCatId: String!){
@@ -29,6 +31,8 @@ interface Product{
   img: string
 }
 
+const fahim = [1,2,3,4,5,6];
+
 export default function Products() {
 
   const { data } = useQuery(ALL_SUBCATEGORY, { 
@@ -43,7 +47,9 @@ export default function Products() {
 <section className="py-10">
             <div className="container">
                 <div className="grid grid-cols-4 gap-6">
-                {data?.productsBySubCategory.map((ele: Product) => (
+                {
+                 data ? (
+                  data?.productsBySubCategory.map((ele: Product) => (
                     <div className="shadow relative" key={ele._id}>
                     <Link href={`/subcategory/${ele._id}`}>
                         <Image
@@ -64,7 +70,32 @@ export default function Products() {
                     </div>
 
                     </div>
-                ))}
+                ))
+                
+                 ): 
+                 (
+
+
+                  fahim.map(()=> 
+                    <div className="shadow relative">
+                       <Skeleton className="w-full h-[300px]" />
+                 
+
+                    {/* Prodcut wishlist & cart icon  */}
+                    <div className="absolute top-2 right-2">
+                        <CartWishlistIcon ele={""} />
+                    </div>
+                    <div className="p-3 bg-white text-center">
+                       <Skeleton className="w-full h-[25px]" />
+                    </div>
+
+                    </div>
+
+                  )
+
+                 )
+                
+                }
                 </div> 
             </div>
         </section>
